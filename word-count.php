1d2d79a5b5cbea1f57c8ed2750a212ff80d77171
Tanvir <tanvirlaravel@gmail.com>
2024-10-13 13:39:16 +0600

    initail commit

---
+++ word-count.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Plugin Name: Word Count
+ * Plugin URI: https://tanvir-word-count.com
+ * Description: Count word from any wordpress post
+ * Version: 1.0.0
+ * Author: Md Tanvirul Islam
+ * Author URI: tnavir.github
+ * License: GPLv2 or later 
+ * Text Domain: word-count
+ * Domain Path: /languages/
+ */
+
